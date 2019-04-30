8a60123a82ceb4550c12a25deb86b7090ca640a2
Lagdameo <ralphgonzaga@asm-smt.com>
2019-04-30 13:33:42 +0800

    Initial Commit

---
+++ WebAppTest-SPA/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      HttpClientModule,
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
